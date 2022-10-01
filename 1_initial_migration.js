b5d39d6ea8e41a9512c691433a50952a555a2d4d
Gourab Bank <gourabbank12345@gmail.com>
2022-10-01 12:29:53 +0530

    INIT

---
+++ 1_initial_migration.js
@@ -0,0 +1,16 @@
+const Migrations = artifacts.require("Migrations");
+const DaiTokenMock = artifacts.require("DaiTokenMock");
+
+module.exports = async function(deployer) {
+  await deployer.deploy(Migrations);
+  await deployer.deploy(DaiTokenMock);
+  const tokenMock = await DaiTokenMock.deployed()
+  // Mint 1,000 Dai Tokens for the deployer
+  await tokenMock.mint(
+    '0xaCdEF786Fb6115d0D59B6D89e0834197AE4F1DB0',
+    '1000000000000000000000'
+
+
+    
+  )
+};
